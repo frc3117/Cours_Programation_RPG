f0d24e519e4897e75b36348b12a72329045dad38
P-B3117 <zacse38@gmail.com>
2020-05-26 21:08:41 -0300

    player heal damage hp

--- RPG/Class/Player.java
+++ RPG/Class/Player.java
@@ -2,5 +2,48 @@ package RPG.Class;
 
 public class Player 
 {
-    
+    public enum ClassType
+    {
+        Warrior,
+        Wizard
+    }
+    public Player(ClassType playerClass)
+    {
+       switch(playerClass) 
+       {
+         case Warrior : 
+         _maxHp = 20;
+         _manaPoint = 12;
+         break;
+
+         case Wizard :
+         _maxHp = 12;
+         _manaPoint = 20;
+         break;
+       }
+       _currentHp = _maxHp;        
+    }
+
+    private int _maxHp ;
+    private int _currentHp ;
+    private int _armorPoint ;
+    private int _atkPoint ;
+    private int _manaPoint ;
+
+    public int GetHp()
+    {
+        return _currentHp ;
+    } 
+    public void Heal(int heal) 
+    {
+      _currentHp += heal;
+      if (_currentHp > _maxHp) 
+      {
+         _currentHp = _maxHp ;
+      }
+    }
+    public void Damage(int damage)
+    {
+        _currentHp -= damage ;
+    }
 }
--- RPG/Main.java
+++ RPG/Main.java
@@ -1,9 +1,11 @@
 package RPG;
 
+import RPG.Class.Player;
+
 public class Main
 {
     public static void main(String[] args) 
     {
-        
+        Player bob = new Player(Player.ClassType.Warrior) ;
     }
 }
